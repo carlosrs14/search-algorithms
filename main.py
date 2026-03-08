d0877afa5490674f9255052dec4fc049da5e3d71
carlosrs14 <crincones578@gmail.com>
2026-03-08 01:25:02 -0500

    fix: new trying to solve that screen

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
 import asyncio
 import pygame
+from gui.window import Window
+from utils import sizes
 from utils.reader import GraphReader
+from gui.map import Map
 
 async def main():
-    pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
-    
-    # Test Data Loading
+    start_node = 0
+    end_node = 30
     data_file = 'data/chn31.txt'
+
     await asyncio.sleep(0) # Let pygame init
-    
-    try:
-        reader = GraphReader(data_file)
-        cities = reader.get_cities()
-        screen.fill((0, 0, 255)) # Blue screen = data successfully loaded!
-    except Exception as e:
-        print("DATA LOAD FAILED:", e)
-        screen.fill((255, 255, 0)) # Yellow screen = Data error!
-        
-    pygame.display.update()
-    
-    while True:
-        for event in pygame.event.get():
-            pass
-        pygame.display.update()
-        await asyncio.sleep(0)
+    reader = GraphReader(data_file)
+    cities = reader.get_cities()
+    graph = reader.get_graph()
+
+    await asyncio.sleep(0) # Yield before creating heavy window
+    game_map = Map(cities, sizes.REGULAR[0], sizes.REGULAR[1])
+    window = Window(
+        sizes.REGULAR, 
+        "Algorithm Visualizer", 
+        game_map=game_map, 
+        graph=graph, 
+        cities=cities, 
+        start_node=start_node, 
+        end_node=end_node
+    )
+    await window.start()
 
 if __name__ == "__main__":
     asyncio.run(main())
